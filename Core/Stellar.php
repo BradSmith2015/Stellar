78d61ae739cb89572622c508d07969ba7f64bfd7
Henok Hailemariam <hennymaster1@gmail.com>
2017-12-20 16:44:52 -0700

    Stellar Class
    Handles all the operation like adding an artist to the database

--- Core/Stellar.php
+++ Core/Stellar.php
@@ -1,22 +1,40 @@
 <?php 
-	namespace Core;
-	use Database;
-
+	use User\User;
+	use User\UserError;
+	use Config\Database;
+	
 class Stellar extends User{
 	
-	public __construct(){
-		$this->db = new Database(1)
+	function __construct(){
+		$this->db = new Database(1);
 	}
 	
 	public function getSeed(){
 		
 	}
-	
-	public function addArtistToLikedList($artist){
-		$stmt = $this->db->query("insert into `table` where");
+	public function getLikedArtists($email){
+		$stmt = $this->db->query("select * from `liked_artists` where email = ?", [$email]);
+			return $stmt;
 	}
 	
-	public function addTrackToLikedList($track_id){
-		
+	public function getLikedSongs($email){
+		$stmt = $this->db->query("select * from `liked_songs` where email = ?", [$email]);
+			return $stmt;
+	}
+	
+	public function addArtistToLikedList($email, $artist, $url){
+		$stmt = $this->db->query("insert into `liked_artists` (email, artist, url) values(?,?,?)", [$artist, $email, $url]);;
+		if($stmt){
+			return true;
+		}
+		return false;
+	}
+	
+	public function addTrackToLikedList($email, $song_url, $title, $artist_name){
+		$stmt = $this->db->query("insert into `liked_songs` (email, artist, url) values(?,?,?)", [$song_url, $title, $artist_name]);;
+		if($stmt){
+			return true;
+		}
+		return false;
 	}
 }
